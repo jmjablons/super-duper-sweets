3f630e52cda0df444719a07fea8bfeca4f8dfc29
jmjablons <juda.jablonska@gmail.com>
2020-01-22 16:49:15 +0100

    init commit

---
+++ R/user.R
@@ -0,0 +1,72 @@
+
+# usage -------------------------------------------------------------------
+# load dependencies
+# read whats going on here
+# load data [import()]:
+#   run the line and choose files from a file tree
+#   or: provide vector of paths to files as an arg to the function and run
+# decide on how to transform the resultss
+# run a summary
+# save results to a file (function not written, do you need it?)
+
+# run ---------------------------------------------------------------------
+# first step - import and parse data
+#   calls WIN windows
+
+# reads a single file
+#   returns a list of data frames
+#dData <- wrap()
+
+# reads chosen files
+#   returns a list of data frames
+dBig <- import()
+
+# dependency
+library(dplyr)
+
+# filters by msn and transforms to data frame
+#   %>% is a pipe, a stream, to avoid temporary variables
+dITI <- filtermsn(dBig, "RAT_PRL_80_20_vPele_5s_ITI") %>%
+  dplyr::bind_rows(.id = "sessionfile")
+
+#View(dITI)
+
+# analysis ----------------------------------------------------------------
+# mail:
+# rewards, 
+# %correct, 
+# reversals, 
+# omissions, 
+# Win.Stay.Correct, 
+# Lose.Shift.Correct, 
+# Latency.Correct, 
+# Win.Stay.InCorrect, 
+# Lose.Shift.InCorrect,
+# Latency.InCorrect, 
+# ulamek.Win.Stay, 
+# ulamek.Lose.Shift
+
+# dependency
+library(dplyr)
+
+# summary
+summary <- dITI %>%
+  mutate(
+    correct = ifelse(!is.na(choice) & choice == correctlever, 1, 0),
+    time = as.POSIXct(paste0(startDate, startTime, sep = ' '), 
+                      format="%m/%d/%y %H:%M:%S")) %>%
+  group_by(subject) %>%
+  arrange(time, trial, .by_group = TRUE) %>%
+  mutate(
+    stay = ifelse(choice == lead(choice), 1, 0)) %>% #omission?
+  ungroup() %>%
+  group_by(subject, startDate = deblank(startDate)) %>%
+  summarise(
+    nNAstay = length(which(is.na(stay))),
+    trials = length(unique(trial)),
+    `%correct` = sum(correct, na.rm = T)/trials, #omission as 'incorrect'
+    rewards = sum(reward, na.rm = T),
+    omissions = length(choice[choice %in% -1]),
+    reverals = length(which(lag(correctlever) != correctlever)))
+
+View(summary)
--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# super-duper-sweets
-read transform and summarise datasets
+# usage
+
+## import data
+
+choose from a directory
+```{r}
+mydata <- import()
+```
+or provide paths
+```{r}
+mydata <- import(c("~/data/file1","~/data/file2"))
+```
+
+## transform
+
+filter by a msn and return list
+
+```{r}
+mydata_prl <- filtermsn(dBig, "RAT_PRL_80_20_vPele_5s_ITI")
+```
+and then it's better to make it a data frame
+
+```{r}
+#library(dplyr)
+mydata_prl = dplyr::bind_rows(mydata_prl)
+```
+
+## summarise
+
+as you please, mine attempts are in the `user.R` file
